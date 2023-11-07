b99a25639710199b438162f9d9b9c45802bfabac
adriano <slipkaze123@tuta.io>
2023-11-07 19:26:33 -0300

    update visualization

--- movies.jl
+++ movies.jl
@@ -3,14 +3,19 @@
 #Pkg.add("CSV");
 #Pkg.add("Plots");
 #Pkg.precompile();
-using DataFrames, CSV, Plots, Graphs
-function read()
+using DataFrames, CSV, Plots
+function readcsv() # leitura do csv
     path = "data.csv"
     dataframe = DataFrame(CSV.File(path))
 end
-dataframe = read()
+dataframe = readcsv()
 function table()
         
+end
+function filtro_table(a::Int64, b::Int64, cols::Vector{SubString{String}}) # faz o plot dos dados em dataframe
+    col=dataframe[a:b, cols]
+    return col
+    
 end
 function filtro_table(a::Int64, b::Int64, cols::Vector{String})
     col=dataframe[a:b, cols]
@@ -18,5 +23,11 @@ function filtro_table(a::Int64, b::Int64, cols::Vector{String})
     
 end
 println(dataframe[1:3, [:id]])
-println(filtro_table(1, 3, ["id", "Movie Name"]))
+#text = readline()
+text = "id, Movie Name, Year of Release"
+println(text)
+text = split(text, ",")
+text = strip.(text)
+println(text)
+println(filtro_table(1, 3, text))
 
